5ea26efcfa217019fdbc8e547f640d55cb0c33d6
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-06-19 13:05:43 -0400

    personal  site

---
+++ index.js
@@ -0,0 +1,24 @@
+// Grabbing elements from the html doc
+const python = document.getElementById("python");
+const java = document.getElementById("java");
+const docker = document.getElementById("docker");
+const react = document.getElementById("react");
+const javascript = document.getElementById("javascript");
+const sql = document.getElementById("sql");
+const swift = document.getElementById("swift");
+// FINISHED
+// Event Handlers
+python.onclick = () => displayBio("python");
+java.onclick = () => displayBio("java");
+docker.onclick = () => displayBio("docker");
+react.onclick = () => displayBio("react");
+javascript.onclick = () => displayBio("javascript");
+sql.onclick = () => displayBio("sql");
+// swift.onclick = () => console.log("cool");
+
+const displayBio = value => {
+  const id = document.querySelector(`#${value}dc`);
+  console.log(id)
+  id.classList.remove("invis")
+  id.classList.add("visable");
+};
