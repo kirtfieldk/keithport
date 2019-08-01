318bf9eb43cf7912a3cf5588f78735383c2cd82e
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-08-01 16:15:29 -0400

    major edits

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-// Grabbing elements from the html doc
-const python = document.getElementById("python");
-const java = document.getElementById("java");
-const docker = document.getElementById("docker");
-const react = document.getElementById("react");
-const javascript = document.getElementById("javascript");
-const sql = document.getElementById("sql");
-const swift = document.getElementById("swift");
-// FINISHED
-// Event Handlers
-python.onclick = () => displayBio("python");
-java.onclick = () => displayBio("java");
-docker.onclick = () => displayBio("docker");
-react.onclick = () => displayBio("react");
-javascript.onclick = () => displayBio("javascript");
-sql.onclick = () => displayBio("sql");
-// swift.onclick = () => console.log("cool");
+const projects = document.querySelector("#projects");
+const wordShow = document.querySelector("#word-show");
+var y;
+// Array of buzz words
+const buzzing = [
+  "Web Designer",
+  "Full Stack Web and Mobil Developer",
+  "Blogger",
+  "Enviormentalist"
+];
+let i = 0;
+const displayWord = () => {
+  console.log(i);
+  if (i === 3) {
+    i = 0;
+  }
+  wordShow.innerHTML = buzzing[i];
+  i += 1;
+};
 
-const displayBio = value => {
-  const id = document.querySelector(`#${value}dc`);
-  console.log(id)
-  id.classList.remove("invis")
-  id.classList.add("visable");
-}
+setInterval(displayWord, 2000);
+// wordShow.innerHTML = "";
