7e2c5fefb0f195aed6edaefed04d6df9caee2adf
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-08-05 00:34:52 -0400

    edit

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const youtube = document.querySelector("#youtube");
 const earthsis = document.querySelector("#earthsis");
 const employee = document.querySelector("#employee");
 const projectBlock = document.querySelector("#projects");
+// Exits
+const earthLinkExit = document.querySelector("#earthlink-exit");
+const youtubeExit = document.querySelector("#youtube-exit");
+const earthsisExit = document.querySelector("#earthsis-exit");
+const employeeExit = document.querySelector("#employee-exit");
 
 const earthLinkCard = document.querySelector("#earthlink-card");
 const youtubeCard = document.querySelector("#youtube-card");
@@ -57,20 +62,64 @@ setInterval(displayWord, 500);
 
 earthLink.addEventListener("click", () => {
   earthLinkCard.classList.remove("hidden");
-
+  earthLinkCard.classList.add("z-up");
   proj1.classList.add("growth");
-  proj2.classList.add("left-growth");
-  console.log("called");
+  
+  proj2.classList.contains("left-growth")
+    ? proj2.classList.add("")
+    : proj2.classList.add("left-growth");
+  if (proj1.classList.contains("shrink-left"))
+    proj1.classList.remove("shrink-left");
+  if (proj2.classList.contains("shrink-right"))
+    proj2.classList.remove("shrink-right");
+
 });
 youtube.addEventListener("click", () => {
   youtubeCard.classList.add("z-up");
+  proj2.classList.add("left-growth");
   youtubeCard.classList.remove("hidden");
+  if (proj2.classList.contains("shrink-right")) {
+    proj2.classList.remove("shrink-right");
+  }
+  proj1.classList.contains("shrink-left")
+    ? proj1.classList.remove("shrink-left")
+    : proj1.classList.add("");
+  proj1.classList.contains("growth")
+    ? proj1.classList.add("")
+    : proj1.classList.add("growth");
 });
+
 earthsis.addEventListener("click", () => {
   earthsisCard.classList.add("z-up");
   earthsisCard.classList.remove("hidden");
+  earthsisCard.classList.add("fade-grow");
 });
 employee.addEventListener("click", () => {
   employeeCard.classList.remove("hidden");
+  employeeCard.classList.add("z-up");
   proj3.classList.add("growth");
+});
+earthLinkExit.addEventListener("click", () => {
+  earthLinkCard.classList.add("hidden");
+  if (!youtubeCard.classList.contains("hidden")) {
+  } else {
+    proj1.classList.add("shrink-left");
+    proj2.classList.add("shrink-right");
+    proj1.classList.remove("growth");
+    proj2.classList.remove("left-growth");
+  }
+});
+youtubeExit.addEventListener("click", () => {
+  youtubeCard.classList.add("hidden");
+  if (earthLinkCard.classList.contains("hidden")) {
+    if (proj1.classList.contains("growth")) {
+      proj1.classList.remove("growth");
+      proj1.classList.add("shrink-left");
+    }
+    proj2.classList.remove("left-growth");
+    proj2.classList.add("shrink-right");
+  }
+});
+earthsisExit.addEventListener("click", () => {
+  earthsisCard.classList.add("hidden");
 });
